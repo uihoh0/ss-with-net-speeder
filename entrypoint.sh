200fad0c97076583d780d4987dc20e0847a85d27
uihoh0 <uihoh0@gmail.com>
2017-02-07 17:43:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-swaks --from ${FROM_THIS_MAIL} --to ${TO_THIS_MAIL} --server ${SMTPSERVER} -au ${FROM_THIS_MAIL} -ap ${MAIL_PASSWORD} --body iamthe best --h-Subject 12345&nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
+nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 /usr/local/bin/ssserver "$@" 
