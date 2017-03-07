adaae8de8f650fd23c505af9d3405b85711a9e33
uihoh0 <uihoh0@gmail.com>
2017-03-07 11:03:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
+if [[ -z "$TOKEN" ]] || [[ -z "$SECRET" ]] || \
+   [[ -z "$ENDPOINT"]]; then
+   ehco "No config of Arukas.io"
+   echo "Not checking the status of cantainers"
+   addr="none"
+   port="none"
+else
+   raw="curl -s -u $TOKEN:$SECRET https://app.arukas.io/api/containers -H 'Content-Type: application/vnd.api+json' -H 'Accept: application/vnd.api+json'"
+   json=`$raw | jq ".data[]?.attributes | select(.end_point==\"$Endpoint\")
+   addr=`echo $json | jq .host | tr -d '""' `
+   port=`echo $json | jq .service_port `
+fi
 if [[ -z "$FROM_THIS_MAIL" ]] || [[ -z "$SMTPSERVER" ]] || [[ -z "$TO_THIS_MAIL" ]] || \
    [[ -z "$MAIL_PASSWORD" ]]; then
     echo "SERVER, FROM, TO, RUNS, or JOBS are missing"
@@ -13,7 +24,7 @@ else
     #jobs=$JOBS
 fi
 runs="nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 & /usr/local/bin/ssserver \"$@\" "
-swakscmd="swaks --server $server -au $from -ap $password --from $from --to $to -tls --h-Subject hi --body i'm the best "
+swakscmd="swaks --server $server -au $from -ap $password --from $from --to $to -tls --h-Subject hi --body Addr:$addr \\n Port:$port "
 #swakscmd="swaks -4 --tls-optional --server $server --from $from --to $to"
 $swakscmd
 exit 2
